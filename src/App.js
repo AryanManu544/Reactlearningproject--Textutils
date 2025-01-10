3a7380c26cfb9a760cb86097d6681a5b80199295
Aryan Manu <aryanmanu544@gmail.com>
2025-01-10 10:59:58 +0530

    Added some more buttons and dark mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   <div className='container my-3'>
   <Textform heading = "Enter text to analyze below"/>
   </div>
+  <About/>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 export default function Textform(props){
-    const [text,setText] = useState("Enter text here");
+    const [text,setText] = useState("");
     const handleUpclick = () => {
         let newtext = text.toUpperCase()
         setText(newtext)
@@ -9,13 +9,58 @@ export default function Textform(props){
     const handleonChange = (event) => {
         setText(event.target.value)
     }
+    const handleLoclick = () => {
+        let newtext = text.toLowerCase()
+        setText(newtext)
+    }
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+    }
+    const title = () => {
+        let newText = text
+            .split(" ")
+            .map((el) => el.charAt(0).toUpperCase() + el.slice(1).toLowerCase())
+            .join(" ");
+        setText(newText)
+    }
+    const handlecopy = () =>{
+        var text = document.getElementById("myBox")
+        text.select()
+        navigator.clipboard.writeText(text.value)
+    }
+    const handleclear = () =>{
+        var text = document.getElementById("myBox")
+        if (text) {
+            text.value = ""; 
+        } else {
+            console.error("Element with ID 'myBox' not found.");
+        }
+    }
+
     return(
-        <div>
+        <>
+        <div className = "container">
             <h1>{props.heading}</h1>
-            <div class="mb-3">
-            <textarea className="form-control" id="myBox" value= {text} onChange= {handleonChange} rows="3"></textarea>
+            <div className="mb-3">
+            <textarea className="form-control" id="myBox" value= {text} onChange= {handleonChange} rows="8"></textarea>
             </div>
             <btn className="btn btn-primary" onClick = {handleUpclick}>Convert to Uppercase</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handleLoclick}>Convert to Lowercase</btn>
+            <button type="submit" onClick={speak} className="btn btn-primary">Speak</button>
+            <btn className="btn btn-primary mx-2" onClick = {title}>Capitalize the words</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handlecopy}>Copy Text</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handleclear}>Clear Text</btn>
+
         </div>
+        <div className= "container my-3" >
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} Words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
