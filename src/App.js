fe22b801f1a0b409dc1a295146420aeeb4883133
Aryan Manu <aryanmanu544@gmail.com>
2025-01-11 13:15:00 +0530

    enhanced dark mode

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-import About from './components/About';
+//import About from './components/About' <About/>;
+import { useState } from 'react';
 
 function App() {
+  const [mode,setmode] = useState("light")
+  const togglemode =() => {
+    if (mode === "light"){
+      setmode('dark')
+      document.body.style.backgroundColor = "black"
+    }
+    else{
+      setmode('light')
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
     <>
-  <Navbar title = "TextUtils" aboutText = "About TextUtils"/>
+  <Navbar title = "TextUtils" aboutText = "About TextUtils" mode= {mode} togglemode= {togglemode}/>
   <div className='container my-3'>
-  <Textform heading = "Enter text to analyze below"/>
+  <Textform heading = "Enter text to analyze below" mode= {mode}/>
   </div>
-  <About/>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Proptypes from "prop-types"
 
 export default function Textform(props){
     const [text,setText] = useState("");
@@ -41,25 +42,25 @@ export default function Textform(props){
 
     return(
         <>
-        <div className = "container">
+        <div className = "container" style= {{color: props.mode==="dark"?"white":"black"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="myBox" value= {text} onChange= {handleonChange} rows="8"></textarea>
+            <textarea className="form-control" id="myBox" style= {{backgroundColor: props.mode==="dark"?"black":"white", color: props.mode==="dark"?"white":"black"}} value= {text} onChange= {handleonChange} rows="8"></textarea>
             </div>
-            <btn className="btn btn-primary" onClick = {handleUpclick}>Convert to Uppercase</btn>
-            <btn className="btn btn-primary mx-2" onClick = {handleLoclick}>Convert to Lowercase</btn>
-            <button type="submit" onClick={speak} className="btn btn-primary">Speak</button>
-            <btn className="btn btn-primary mx-2" onClick = {title}>Capitalize the words</btn>
-            <btn className="btn btn-primary mx-2" onClick = {handlecopy}>Copy Text</btn>
-            <btn className="btn btn-primary mx-2" onClick = {handleclear}>Clear Text</btn>
+            <btn className="btn btn-primary" onClick = {handleUpclick} style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Convert to Uppercase</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handleLoclick} style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Convert to Lowercase</btn>
+            <button type="submit" onClick={speak} className="btn btn-primary" style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Speak</button>
+            <btn className="btn btn-primary mx-2" onClick = {title} style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Capitalize the words</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handlecopy} style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Copy Text</btn>
+            <btn className="btn btn-primary mx-2" onClick = {handleclear} style= {{backgroundColor: props.mode === "dark"?"#fd7e14":"blue"}}>Clear Text</btn>
 
         </div>
-        <div className= "container my-3" >
+        <div className= "container my-3" style= {{color: props.mode==="dark"?"white":"black"}}>
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} Words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
         </div>
         </>
     )
